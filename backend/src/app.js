7bb3125263cbc3a6437ddabfb1646373bc3f8961
Rahul Saini <rahulsaini2522@gmail.com>
2026-02-17 12:32:28 +0530

    update file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,40 @@
-import express from 'express';
 
+import express from "express";
 import { createServer } from "node:http";
 
 import { Server } from "socket.io";
 
-import cors from 'cors';
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
 
-import mongoose from 'mongoose';
-
-import connectToSocket from './controllers/socketManager.js';
-
-import userRoutes from './routes/users.routes.js';
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
+
 app.set("port", (process.env.PORT || 8000))
 app.use(cors());
-app.use(express.json({limit: '40kb'}));
-app.use(express.urlencoded({extended: true, limit: '40kb'}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.use("/api/v1/users", userRoutes); 
+app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-    const connectionDb = await mongoose.connect("mongodb+srv://rahulsaini2522_db_user:haribol1@nexlive.zfupqly.mongodb.net/?appName=NexLive")
-    console.log('Mongo connected DB Host: ', connectionDb.connection.host)
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
-        console.log("Server is running on port " + app.get("port"))
+        console.log("LISTENIN ON PORT 8000")
     });
+
+
+
 }
+
+
 
 start();
